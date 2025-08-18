203e28d241aab0241558c426ea80d066760eda22
xxjay <760409558@qq.com>
2025-08-18 21:10:18 +0800

    fix: 接口定义错误

--- frontend/src/api/system/user.api.js
+++ frontend/src/api/system/user.api.js
@@ -2,7 +2,7 @@ import http from "@/api/index.js";
 
 export const queryUserList = (params)=>http.get("/user/list", {params: params});
 
-export const addUser = (data) => http.post('/user/add', data);
+export const addUser = (data) => http.post('/user/create', data);
 
 export const updateUser = (data) => http.put('/user/update', data);
 
